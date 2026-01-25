64e08264ec1cdd49f41b30615413f197dace15c8
elf.ast.ahe <elf.ast.ahe@gmail.com>
2026-01-25 19:58:48 +0300

    dev: iter 19, fixes

--- internal/dal/repository/db_conn_check_inmem_repo_test.go
+++ internal/dal/repository/db_conn_check_inmem_repo_test.go
@@ -14,12 +14,14 @@ func TestInMemCheckDBConn_ShouldReturnNull(t *testing.T) {
 	require.NoError(t, err)
 	// act
 	t.Run("with context", func(t *testing.T) {
-		actual := repo.CheckDBConn(context.Background())
+		var ctx = context.Background()
+		actual := repo.CheckDBConn(ctx)
 		assert.Nil(t, actual)
 	})
 	// act
 	t.Run("without context", func(t *testing.T) {
-		actual := repo.CheckDBConn(nil)
+		var ctx context.Context = nil
+		actual := repo.CheckDBConn(ctx)
 		assert.Nil(t, actual)
 	})
 }
